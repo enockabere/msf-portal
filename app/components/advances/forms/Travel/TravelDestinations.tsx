d24f7e63f75e2a506ca833c5d61f4750b096fe9c
wemali05 <alfredwemali05@gmail.com>
2025-05-16 19:14:25 +0300

    visa and travel checklist progress

--- app/components/advances/forms/Travel/TravelDestinations.tsx
+++ app/components/advances/forms/Travel/TravelDestinations.tsx
@@ -4,7 +4,7 @@ import React, {useEffect, useState} from "react";
 import DataTable, { TableColumn } from "react-data-table-component";
 import { Save, Trash2 } from "lucide-react";
 import { useMySetups } from "@/app/context/SetupContext";
-import {createResource, getResource} from "@/app/lib/api/http";
+import {createResource, deleteResource, getResource} from "@/app/lib/api/http";
 import {  Destination } from "@/app/types/Destination";
 import {Approval} from "@/app/types/approval";
 import Swal from "sweetalert2";
@@ -58,7 +58,6 @@ export default function TravelDestinations({
 
       if(res.error) {
          return Swal.fire('Error!', res.error.message)
-
       }
       console.log('create routes res', res)
       Swal.fire("Success", 'Travel route was created successfully!' );
@@ -67,6 +66,26 @@ export default function TravelDestinations({
     }
 
   }
+
+    const deleteTravelRequest = async (row: Destination) => {
+        try {
+            const res =  await deleteResource('travelRoutes', {
+                params: {
+                    documentType: "",
+                    documentNo: "",
+                    sequenceNo: ""
+                }
+            });
+
+            if(res.error) {
+                return Swal.fire('Error!', res.error.message)
+            }
+            console.log('create routes res', res)
+            Swal.fire("Success", 'Travel route was deleted successfully!' );
+        } catch (e) {
+            Swal.fire('Error!', e.message)
+        }
+    }
 
   const fetchCities = async (countryCode, countryField) => {
     try {
@@ -120,7 +139,7 @@ export default function TravelDestinations({
     const userTravelRoutes = async () => {
        const res = await getResource('travelRoutes', {
             params: {
-                filter: {
+                filters: {
                     documentNo: "ETR003"
                 }
             }
@@ -161,7 +180,7 @@ export default function TravelDestinations({
                         >
                             <option value="">-- Origin City --</option>
                             {originCities.map((city) => (
-                                <option key={city.code} value={city.code}>
+                                <option key={city.code} value={city.city}>
                                     {city.city}
                                 </option>
                             ))}
@@ -194,7 +213,7 @@ export default function TravelDestinations({
                         >
                             <option value="">-- Destination City --</option>
                             {destinationCities.map((city) => (
-                                <option key={city.code} value={city.code}>
+                                <option key={city.code} value={city.city}>
                                     {city.city}
                                 </option>
                             ))}
@@ -304,14 +323,15 @@ export default function TravelDestinations({
         },
         {
             name: "Actions",
-            cell: (row: Approval) => (
+            cell: (row: Destination) => (
                 <div className="d-flex gap-2">
                     <button
-                        className="text-success border-0 bg-transparent"
-                        title="View"
-                        onClick={() => (row)}
+                        type="button"
+                        className="btn btn-outline-danger btn-sm"
+                        onClick={() => deleteTravelRequest(row)}
+                        title="Delete"
                     >
-                        <i className="las la-eye fs-18" />
+                        <Trash2 size={16} />
                     </button>
                 </div>
             ),
---
+++ app/components/advances/forms/Travel/TravellerChecklist.tsx
@@ -0,0 +1,148 @@
+import React, {useEffect, useState} from "react";
+import {Save, UploadCloud} from "lucide-react";
+import {Form} from "react-bootstrap";
+import {createResource, getResource} from "@/app/lib/api/http";
+import Swal from "sweetalert2";
+
+export default function TravellerChecklist() {
+    const [travelChecklist, setTravelChecklist] = useState([])
+
+    const [rows, setRows] = useState(
+        Array.from({ length: 5 }, (_, index) => ({
+            id: index + 1,
+            expiryDate: '',
+            file: null,
+            checked: false,
+        }))
+    );
+
+    const handleInputChange = (index, value) => {
+        const updatedRows = [...rows];
+        updatedRows[index].expiryDate = value;
+        setRows(updatedRows);
+    };
+
+    // const handleFileChange = (index, file) => {
+    //     const updatedRows = [...rows];
+    //     updatedRows[index].file = file;
+    //     setRows(updatedRows);
+    // };
+
+    const handleCheckboxChange = (index, value) => {
+        const updatedRows = [...rows];
+        updatedRows[index].checked = value;
+        setRows(updatedRows);
+    };
+
+    const saveChecklistItem = async (index) => {
+        const rowData = rows[index];
+
+        try {
+            const res = await createResource('travelRoutes', {
+                data: {
+                },
+            });
+
+            if(res.error) {
+                return Swal.fire('Error!', res.error.message)
+            }
+            console.log('create routes res', res)
+            Swal.fire("Success", 'Travel checklist updated successfully!' );
+        } catch (e) {
+            Swal.fire('Error!', e.message)
+        }
+    };
+
+
+    const getTravelChecklist = async () => {
+        const res = await getResource('travellerChecklist', {
+            params: {
+                filters: {
+                    documentNo: "ETR003",
+                    documentType: "Employee",
+                    checklistType: "Travel",
+                }
+            }
+        })
+
+        console.log('getTravelChecklist', res?.value)
+
+        setTravelChecklist(res?.value || [])
+    }
+
+    useEffect(() => {
+        getTravelChecklist()
+    }, []);
+
+    return (
+        <>
+            <div className='row g-3'>
+                <div className={'col-12'}>
+                    <table className="table table-hover caption-top my-2 align-middle">
+                        <caption className={'text-gray-800'}>Travel checklist items</caption>
+                        <thead className="table-light">
+                        <tr>
+                            <th>Item - Description</th>
+                            <th>Expiry Date</th>
+                            {/*<th>Attach</th>*/}
+                            <th>Verify</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        {travelChecklist.map((row, index) => (
+                            <tr key={`checklist-item-${index}`}>
+                                <td>{index + 1}. {row.checklistItem}-{row.checklistItemDescription}</td>
+                                <td>
+                                    <input
+                                        type="data"
+                                        className="form-control"
+                                        value={row.expiryDate}
+                                        onChange={(e) => handleInputChange(index, e.target.value)}
+                                        placeholder="Enter ID or Passport number"
+                                        required
+                                    />
+                                </td>
+                                {/*<td>*/}
+                                {/*    <label className="btn btn-sm btn-outline-secondary w-100">*/}
+                                {/*        <UploadCloud size={14} className="me-1" /> Upload*/}
+                                {/*        <input*/}
+                                {/*            type="file"*/}
+                                {/*            accept="image/*,.pdf"*/}
+                                {/*            hidden*/}
+                                {/*            onChange={(e) =>*/}
+                                {/*                handleFileChange(index, e.target.files?.[0] || null)*/}
+                                {/*            }*/}
+                                {/*        />*/}
+                                {/*    </label>*/}
+                                {/*    {row.file && <small>{row.file.name}</small>}*/}
+                                {/*</td>*/}
+                                <td>
+                                    <Form.Check
+                                        type="checkbox"
+                                        id={`check-${index}`}
+                                        className="mb-2 text-capitalize"
+                                        checked={row.has}
+                                        onChange={(e) => handleCheckboxChange(index, e.target.checked)}
+                                    />
+                                </td>
+                                <td>
+                                    <button
+                                        type="button"
+                                        className="btn btn-outline-success btn-sm"
+                                        onClick={() => saveChecklistItem(index)}
+                                        title="Save"
+                                    >
+                                        <Save size={16} />
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+    );
+}
