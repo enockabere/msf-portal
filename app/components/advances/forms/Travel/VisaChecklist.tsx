b88fdc2aaf33e69bf71602c3804c51226bed96a7
wemali05 <alfredwemali05@gmail.com>
2025-05-17 17:54:26 +0300

    update checklist records

--- app/components/advances/forms/Travel/VisaChecklist.tsx
+++ app/components/advances/forms/Travel/VisaChecklist.tsx
@@ -4,58 +4,10 @@ import { Form } from 'react-bootstrap';
 import {createResource, getResource} from "@/app/lib/api/http";
 import Swal from "sweetalert2";
 import {TravelRequest} from "@/app/types/travel";
+import ChecklistRow from "@/app/components/travel/ChecklistRow";
 
 export default function VisaChecklist ({travelInfo}: {travelInfo: TravelRequest}) {
     const [visaChecklist, setVisaChecklist] = useState([])
-
-    const [rows, setRows] = useState(
-        Array.from({ length: 5 }, (_, index) => ({
-            id: index + 1,
-            expiryDate: '',
-            file: null,
-            checked: false,
-        }))
-    );
-
-    const handleInputChange = (index, value) => {
-        const updatedRows = [...rows];
-        updatedRows[index].expiryDate = value;
-        setRows(updatedRows);
-    };
-
-    // const handleFileChange = (index, file) => {
-    //     const updatedRows = [...rows];
-    //     updatedRows[index].file = file;
-    //     setRows(updatedRows);
-    // };
-
-    const handleCheckboxChange = (index, value) => {
-        const updatedRows = [...rows];
-        updatedRows[index].checked = value;
-        setRows(updatedRows);
-    };
-
-    const saveChecklistItem = async(index) => {
-        const rowData = rows[index];
-        console.log('Saving row:', rowData);
-        // Example: send to API or process further
-
-        try {
-            const res = await createResource('travelRoutes', {
-                data: {
-                },
-            });
-
-            if(res.error) {
-                return Swal.fire('Error!', res.error.message)
-            }
-            console.log('create routes res', res)
-            Swal.fire("Success", 'Visa checklist updated successfully!' );
-        } catch (e) {
-            Swal.fire('Error!', e.message)
-        }
-    };
-
 
     const getVisaChecklist = async () => {
         const res = await getResource('travellerChecklist', {
@@ -108,47 +60,8 @@ export default function VisaChecklist ({travelInfo}: {travelInfo: TravelRequest}
                                 </thead>
 
                                 <tbody>
-                                {items.map((row, index) => (
-                                    <tr key={`${travellerName}-${row.checklistItem}`}>
-                                        <td>{row.checklistItem}. {row.checklistItemDescription}</td>
-                                        <td>
-                                            <input
-                                                type="date"
-                                                className="form-control"
-                                                value={row.expiryDate}
-                                                onChange={(e) => handleInputChange(
-                                                    editableChecklist.findIndex(i => i.id === row.id),
-                                                    e.target.value
-                                                )}
-                                                placeholder="Enter expiry date"
-                                                required
-                                            />
-                                        </td>
-                                        <td>
-                                            <Form.Check
-                                                type="checkbox"
-                                                id={`check-${travellerName}-${row.checklistItem}`}
-                                                className="mb-2 text-capitalize"
-                                                checked={row.has}
-                                                onChange={(e) => handleCheckboxChange(
-                                                    editableChecklist.findIndex(i => i.id === row.id),
-                                                    e.target.checked
-                                                )}
-                                            />
-                                        </td>
-                                        <td>
-                                            <button
-                                                type="button"
-                                                className="btn btn-outline-success btn-sm"
-                                                onClick={() => updateChecklistItem(
-                                                    editableChecklist.findIndex(i => i.id === row.id)
-                                                )}
-                                                title="Save"
-                                            >
-                                                <Save size={16} />
-                                            </button>
-                                        </td>
-                                    </tr>
+                                {Array.isArray(items) && items.map((row, index) => (
+                                  <ChecklistRow key={row.lineNo + row.checklistItem} row={row}/>
                                 ))}
                                 </tbody>
                             </table>
