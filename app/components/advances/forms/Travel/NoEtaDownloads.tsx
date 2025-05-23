4b6b977439e9143f30e7311281824899083c7d3e
enock abere <enock.abere@kinetics.co.ke>
2025-05-23 11:29:46 +0300

    voucher download changes

--- app/components/advances/forms/Travel/NoEtaDownloads.tsx
+++ app/components/advances/forms/Travel/NoEtaDownloads.tsx
@@ -67,7 +67,7 @@ const NoEtaDownloads: React.FC<NoEtaDownloadsProps> = ({
       }
 
       const link = document.createElement("a");
-      link.href = `data:application/pdf;base64,${data.downloadUrl}`;
+      link.href = `data:application/pdf;base64,${data.downloadUrl.value}`;
       link.download = `LetterOfInvitation_${primaryKey.no}.pdf`;
       link.target = "_self";
       link.rel = "noopener";
