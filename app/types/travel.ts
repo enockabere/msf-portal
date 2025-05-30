e0e327fb6104cd58fb3158e8318af687668cc5de
pkaruga <peterkaruga3@gmail.com>
2025-05-30 09:26:53 +0300

    Cleanup

--- app/types/travel.ts
+++ app/types/travel.ts
@@ -1,8 +1,3 @@
-export interface TravelDates {
-  from: string;
-  to: string;
-}
-
 export interface TravelRequest {
   documentType: string;
   no: string;
@@ -26,21 +21,7 @@ export interface TravelRequest {
   shortcutDimension1Code: string;
   shortcutDimension2Code: string;
   budgetCode: string;
-  approvalStatus: 'Open' | string; // Assuming 'Open' is one of possible statuses
-  [key: string]: any;
-}
-
-export interface TravelRequestProviders {
-  documentType: string,
-  documentNo: string,
-  serviceCode: string,
-  serviceProvider: string,
-  serviceDescription: string,
-  vendorName: string,
-  notify: boolean,
-  isTransportation: boolean,
-  vehicleRegistrationNo: string,
-  phoneNo: string,
-
+  approvalStatus: 'Open' | string;
+  hasValidVisa: false;
   [key: string]: any;
 }
